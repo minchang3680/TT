8fe20d0639aaccb59a08f5e6791178acfc1f1a34
minchang3680 <minchang3680@naver.com>
2024-11-29 13:55:23 +0900

    파이썬기초#1

---
+++ 1.py
@@ -0,0 +1,14 @@
+'''
+print("Hello, Would")
+'''
+print("Hello", "Python" , 1, 2, "asdfe",sep=",")
+print()
+print(1)
+print("안녕하세요", end="")
+print("  코딩온입니다.")
+ive = "장원영"
+Ive = "카리나"
+print(ive, Ive)
+im = ("야구")
+im = ("축구")
+print(im)
