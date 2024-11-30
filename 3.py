4df9a0983c5a07074fbe800e5125fbdddd7e4900
minchang3680 <minchang3680@naver.com>
2024-11-30 13:51:12 +0900

    3번째

---
+++ 3.py
@@ -0,0 +1,28 @@
+'''song = input("너의 최애의 노래는 ?")
+print(song)
+print(type(song))
+a = input("1+1=?")
+print(a)
+print(int(a)*2)'''
+# a=2
+# print(str(2)*10)
+# print(str(2)+"입니다")
+# print(2+"입니다") 의 경우 에러가 난다.
+# print(" |\_/|")
+# print(" |q p|   /}")
+# print(' ( 0 )"""\\ ')
+# print(' |"^"\'   |')
+# print(" |_/=\\\__|") #\\ 앞에 \를 하나 더 붙여준다. \를 표현하기 위해서 2개이상의 \를 써주면 된다.
+name = input("이름을 입력하세요.")
+years = input("나이를 입력하세요.")
+print(name)
+print(years) 
+print(f"안녕하세요! {name}님,{years}님 \n")
+
+name = input("이름을 입력하세요.")
+years2 = int(input("태어난 년도를 입력하세요."))
+years3 = int(input("올해 년도를 입력하세요."))
+print(name)
+print(years2) 
+print(years3)
+print(f"올해는 {years3}년, {name}님의 나이는 {years3-years2}세 입니다.")
