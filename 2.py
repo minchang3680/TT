301e95d7896285831b5b43b07381a66dc9973514
minchang3680 <minchang3680@naver.com>
2024-11-30 13:50:54 +0900

    2번째

---
+++ 2.py
@@ -0,0 +1,25 @@
+'''
+print(3//2)
+print(3.25//2)
+print(3%2)
+print(3.25%2)
+print(2**3)
+print(2**10)
+print(1+2*3**2)
+print(1+(2*(3**2)))
+print(1+2*-3**2)
+'''
+print("빵의 개수 :" ,30//4)
+print("남은 빵의 개수 :",30%4)
+
+a=1
+a+= 1 # a=1 a+1
+print(a)
+print("장원영"+"럭키비키")
+print("럭키"*10)
+# print("럭키"*"비키") 문자끼리 곱하면 에러가 난다.
+print("럭키"+"비키")
+print(2023-2010)
+years1 = 2023
+years2 = 2010
+print(years1-years2)
