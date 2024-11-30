004ce3e48c75bdbe9a99d877c64c113ef9ddf42b
minchang3680 <minchang3680@naver.com>
2024-11-30 13:50:33 +0900

    1번째

--- 1.py
+++ 1.py
@@ -1,14 +1,35 @@
-'''
-print("Hello, Would")
-'''
-print("Hello", "Python" , 1, 2, "asdfe",sep=",")
-print()
-print(1)
-print("안녕하세요", end="")
-print("  코딩온입니다.")
-ive = "장원영"
-Ive = "카리나"
-print(ive, Ive)
-im = ("야구")
-im = ("축구")
-print(im)
+# '''
+# print("Hello, Would")
+# print("Hello", "Python" , 1, 2, "asdfe",sep=",")
+# print()
+# print(1)
+# print("안녕하세요", end="")
+# print("  코딩온입니다."}
+# im = ("야구")
+# im = ("축구")
+# print(im)
+# '''
+# ive = "장원영"
+# Ive = "카리나"
+# print(ive, Ive)
+
+# print(f"나는 {ive}입니다")
+# print("나는",ive,"입니다")
+# print("나는 "+ive+"입니다")
+# print(type(77))
+# print(type(7.2))
+# print(type("ive"))
+# print("111'111")
+# num = 10
+# b_num = 0b1010
+# h_num = 0xa
+# print(num)
+# print(b_num)
+# print(h_num)
+# print(bin(10))
+# print(hex(10))
+# print(ord('0'))
+# print(ord('a'))
+# print(chr(48))
+# print(chr(65))
+name = input()
