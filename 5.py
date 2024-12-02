9ecb75a5d0bbf34e9c8ef6f497e6deb2ee9b61df
minchang3680 <minchang3680@naver.com>
2024-12-02 14:00:21 +0900

    다섯 번쨰

---
+++ 5.py
@@ -0,0 +1,23 @@
+# print("나는 이번달에 책을 %d권 샀어요." %(1))
+# print("오늘은 12월 %d일 입니다." %(12.2))
+# print("오늘은 %d%s %d일 입니다" %(12,'월',2))
+# print(f"오늘은 {12}{'월'} {2}일 입니다")
+# print("오늘은 "+str(12)+"월 "+str(2)+"일 입니다")
+# print("오늘은 ",12,"월 ",2,"일")
+# print("hello".upper())
+# print("HELLO".lower())
+# friends ="고찬국 김다운 김민창"
+# a = friends.split("김")
+# print(a)
+# sentence = "{}월 {}일".format(12,2) #12월 2일
+# print(sentence)
+# b = "   111     222     "
+# print(b.strip())
+# print(b.split())
+# a = int(input("첫번쨰 세자리 수를 입력하시오 :")) #472
+# b = input("두번쨰 세자리 수를 입력하시오 :") #385
+# print((a)*int(b[2]))
+# print((a)*int(b[1]))
+# print((a)*int(b[0]))
+# print((a)*int(b))
+
