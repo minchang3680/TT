8c691baa72a606d9682aaecb3f73e75b0cec9c18
minchang3680 <minchang3680@naver.com>
2024-12-04 22:34:01 +0900

    3번째

--- 3.py
+++ 3.py
@@ -19,10 +19,14 @@
 # print(years) 
 # print(f"안녕하세요! {name}님,({years})님")
 
-name = input("이름을 입력하세요.")
-years2 = int(input("태어난 년도를 입력하세요."))
-years3 = int(input("올해 년도를 입력하세요."))
-print(name)
-print(years2) 
-print(years3)
-print(f"올해는 {years3}년, {name}님의 나이는 {years3-years2}세 입니다.")
+# name = input("이름을 입력하세요.")
+# years2 = int(input("태어난 년도를 입력하세요."))
+# years3 = int(input("올해 년도를 입력하세요."))
+# print(name)
+# print(years2) 
+# print(years3)
+# print(f"올해는 {years3}년, {name}님의 나이는 {years3-years2}세 입니다.")
+
+shop = ["a","b","c","d","e"]
+print(shop.index("c")) #2
+print(shop.index("없는 값")) # 존재하지 않는 값 : value error
