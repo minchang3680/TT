ad67e824ae1d9f330af99c17c07a54656b73dd93
minchang3680 <minchang3680@naver.com>
2024-12-07 13:50:07 +0900

    11번째

--- 11.py
+++ 11.py
@@ -64,11 +64,13 @@
 # 문자열 myString이 주어집니다. myString을 문자 "x"를 기준으로 나눴을 때 나눠진 문자열 각각의 길이를 
 # 순서대로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
 
-def solution(myString):
-    answer = []
-    myStringlist = myString.split("x")
-    for i in myStringlist:
-        answer.append(len(i))
-    return answer
-print(solution("oxooxoxxox"))
-    
+# def solution(myString):
+#     answer = []
+#     myStringlist = myString.split("x")
+#     for i in myStringlist:
+#         answer.append(len(i))
+#     return answer
+# print(solution("oxooxoxxox"))
+
+l = ["p","y","t","h","o","n"]
+print("".join(l))
